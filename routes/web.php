c8a7f6df01fa3e70505e02409a154c53d238d71d
prmnaa30 <pramanakadek335@gmail.com>
2024-12-27 19:52:00 +0800

    added admin dashboard and admin layout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\adminPageController;
 use App\Http\Controllers\landingPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [landingPageController::class, 'index'])->name('landing');
+
+Route::get('/admin', [adminPageController::class, 'index'])->name('admin');
