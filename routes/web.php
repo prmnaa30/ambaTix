02beede08cd4ff210d03681057eb4df3ee3e281d
prmnaa30 <pramanakadek335@gmail.com>
2025-01-03 21:19:47 +0800

    modified: events resource route added

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\adminPageController;
+use App\Http\Controllers\eventController;
 use App\Http\Controllers\landingPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [landingPageController::class, 'index'])->name('landing');
 
+/** Admin */
 Route::get('/admin', [adminPageController::class, 'index'])->name('admin');
+Route::resource('/admin/events', eventController::class);
