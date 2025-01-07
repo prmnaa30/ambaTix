0f5bd46b5a179312bb6998e475c3932acc544e01
prmnaa30 <pramanakadek335@gmail.com>
2025-01-07 22:29:54 +0800

    modified: tickets CRUD route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\eventController;
 use App\Http\Controllers\landingPageController;
 use App\Http\Controllers\contentLandingController;
 use App\Http\Controllers\contentPaymentController;
+use App\Http\Controllers\ticketController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [landingPageController::class, 'index'])->name('landing');
@@ -15,3 +16,14 @@ Route::get('/content/content-payment', [contentPaymentController::class, 'index'
 
 /** Admin */
 Route::get('/admin', [adminPageController::class, 'index'])->name('admin');
+Route::resource('events', eventController::class);
+
+// Tickets
+Route::prefix('/events/{eventId}')->name('events.tickets.')->group(function () {
+  Route::get('/tickets', [ticketController::class, 'index'])->name('index');
+  Route::get('/tickets/create', [ticketController::class, 'create'])->name('create');
+  Route::post('/tickets', [ticketController::class, 'store'])->name('store');
+  Route::get('/tickets/{ticketId}/edit', [ticketController::class, 'edit'])->name('edit');
+  Route::put('/tickets/{ticketId}', [ticketController::class, 'update'])->name('update');
+  Route::delete('/tickets/{ticketId}', [ticketController::class, 'destroy'])->name('destroy');
+});
