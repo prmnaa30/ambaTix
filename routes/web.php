9d315c10c5dc4343cdda099956c7c9d71e0b97f0
prmnaa30 <pramanakadek335@gmail.com>
2025-01-12 22:34:54 +0800

    modified: login, register and middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\adminPageController;
+use App\Http\Controllers\auth\loginController;
+use App\Http\Controllers\auth\registerController;
 use App\Http\Controllers\eventController;
 use App\Http\Controllers\landingPageController;
 use App\Http\Controllers\contentLandingController;
@@ -10,20 +12,39 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [landingPageController::class, 'index'])->name('landing');
 
+Route::middleware('guest')->group(function () {
+  Route::get('/register', [registerController::class, 'showRegisterForm'])->name('register');
+  Route::post('/register', [registerController::class, 'register'])->name('register');
+  Route::get('/login', [loginController::class, 'showLoginForm'])->name('login');
+  Route::post('/login', [loginController::class, 'login'])->name('login');
+});
+
+/** Admin */
+Route::middleware(['auth', 'role:admin'])->group(function () {
+  Route::get('/admin', [adminPageController::class, 'index'])->name('admin');
+
+  // Events
+  Route::resource('events', eventController::class);
+
+  // Tickets
+  Route::prefix('/events/{eventId}')->name('events.tickets.')->group(function () {
+    Route::get('/tickets', [ticketController::class, 'index'])->name('index');
+    Route::get('/tickets/create', [ticketController::class, 'create'])->name('create');
+    Route::post('/tickets', [ticketController::class, 'store'])->name('store');
+    Route::get('/tickets/{ticketId}/edit', [ticketController::class, 'edit'])->name('edit');
+    Route::put('/tickets/{ticketId}', [ticketController::class, 'update'])->name('update');
+    Route::delete('/tickets/{ticketId}', [ticketController::class, 'destroy'])->name('destroy');
+  });
+
+  // Transaksi
+  Route::get('/transaksi', [adminPageController::class, 'transaksi'])->name('transaksi');
+
+  // User
+  Route::get('/user', [adminPageController::class, 'user'])->name('user');
+});
+
+Route::get('/logout', [loginController::class, 'logout'])->name('logout');
+
 Route::get('/content', [contentLandingController::class, 'index'])->name('contentLanding');
 
 Route::get('/content/content-payment', [contentPaymentController::class, 'index'])->name('contentPayment');
-
-/** Admin */
-Route::get('/admin', [adminPageController::class, 'index'])->name('admin');
-Route::resource('events', eventController::class);
-
-// Tickets
-Route::prefix('/events/{eventId}')->name('events.tickets.')->group(function () {
-  Route::get('/tickets', [ticketController::class, 'index'])->name('index');
-  Route::get('/tickets/create', [ticketController::class, 'create'])->name('create');
-  Route::post('/tickets', [ticketController::class, 'store'])->name('store');
-  Route::get('/tickets/{ticketId}/edit', [ticketController::class, 'edit'])->name('edit');
-  Route::put('/tickets/{ticketId}', [ticketController::class, 'update'])->name('update');
-  Route::delete('/tickets/{ticketId}', [ticketController::class, 'destroy'])->name('destroy');
-});
