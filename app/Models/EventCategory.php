bf2562628e6dabee2a64ac817acd7c3e3099fb3f
prmnaa30 <pramanakadek335@gmail.com>
2025-01-13 21:14:57 +0800

    added: event category CRUD

--- app/Models/EventCategory.php
+++ app/Models/EventCategory.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class EventCategory extends Model
 {
     protected $table = 'event_categories';
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function events()
+    {
+        return $this->hasMany(Event::class, 'event_categories_id');
+    }
 }
