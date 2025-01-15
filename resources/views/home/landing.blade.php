013960719ad09adcb3b85848a80be3769192d1d3
prmnaa30 <pramanakadek335@gmail.com>
2025-01-16 01:29:25 +0800

    modified: 3 kategori utama

--- resources/views/home/landing.blade.php
+++ resources/views/home/landing.blade.php
@@ -38,18 +38,18 @@
 
     <div class="bg-primary-700 rounded-2xl p-8 px-auto mb-2 just">
         
-        <p class="text-2xl font-bold text-center mb-8">Kategori Event</p>
+        <p class="text-2xl font-bold text-center mb-8">Kategori Utama</p>
         
         <div class="grid grid-cols-3 justify-items-center mb-8">
-            <a href="" class="relative group" id="konser">
+            <a href="{{ route('search', ['id_kategori' => 3]) }}" class="relative group" id="konser">
                 <img src="{{ asset('images/konser.jfif') }}" alt="Event" style="width: 248px; height: 296px; object-fit: cover;" class="rounded-2xl transition-all duration-500 hover:scale-95 hover:opacity-80">
                 <span class="absolute inset-0 flex items-center justify-center text-white text-2xl opacity-0 group-hover:opacity-100 hover:bg-black/50 rounded-2xl transition-opacity duration-500 ">Konser</span>
             </a>
-            <a href="" class="relative group" id="seminar">
+            <a href="{{ route('search', ['id_kategori' => 1])}}" class="relative group" id="seminar">
                 <img src="{{ asset('images/seminar.jfif') }}" alt="Event" style="width: 248px; height: 296px; object-fit: cover;" class="rounded-2xl transition-all duration-500 hover:scale-95 hover:opacity-80">
                 <span class="absolute inset-0 flex items-center justify-center text-white text-2xl opacity-0 group-hover:opacity-100 hover:bg-black/50 rounded-2xl transition-opacity duration-500">Seminar</span>
             </a>
-            <a href="" class="relative group" id="workshop">
+            <a href="{{ route('search', ['id_kategori' => 2]) }}" class="relative group" id="workshop">
                 <img src="{{ asset('images/workshop.jfif') }}" alt="Event" style="width: 248px; height: 296px; object-fit: cover;" class="rounded-2xl transition-all duration-500 hover:scale-95 hover:opacity-80">
                 <span class="absolute inset-0 flex items-center justify-center text-white text-2xl opacity-0 group-hover:opacity-100 hover:bg-black/50 rounded-2xl transition-opacity duration-500">Workshop</span>
             </a>
